8a54d16120d8e4fc3a22ae4b39db3c6e113c7433
Leandro <leandrollemos712@gmail.com>
2024-12-16 23:38:28 -0300

    adjust to server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,8 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 3001,
         EXTERNAL_HOST: "true"
-      }
+      },
+      max_memory_restart: "1G"
     },
     {
       name: "haras-frontend",
@@ -20,7 +21,10 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 3000,
         NEXT_PUBLIC_API_URL: "http://34.56.65.192:3001"
-      }
+      },
+      max_memory_restart: "1G",
+      kill_timeout: 3000,
+      wait_ready: true
     }
   ]
 } 
